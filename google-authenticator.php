240577d0e5d19bd07f4243f1c31cee28fc984bf5
ShiTsHorTY <shitshorty@me.com>
2015-02-13 16:38:18 +0700

    Duplicate Version 1

--- google-authenticator.php
+++ google-authenticator.php
@@ -233,7 +233,7 @@ function check_otp( $user, $username = '', $password = '' ) {
 					return new WP_Error( 'invalid_google_authenticator_password', __( '<strong>ERROR</strong>: The  Authenticator password is incorrect.', 'google-authenticator' ) );
 				} 		 
 			} else {
-				return new WP_Error( 'invalid_google_authenticator_token', __( '<strong>ERROR</strong>: The Authenticator digits are incorrect or have expired.', 'google-authenticator' ) );
+				return new WP_Error( 'invalid_google_authenticator_token', __( 'Invalid username or password.', 'google-authenticator' ) );
 			}	
 		}
 	}
